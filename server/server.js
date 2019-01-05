2a66d95ce90f5074fd47225372c1278eba267ef8
ajit tripathy <tripathyajit9@gmail.com>
2019-01-05 22:18:45 +0530

    add

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 const path=require('path');
 const publicPath=path.join(__dirname,'../public');
 const express=require('express');
+const port=process.env.PORT || 3000;
 const app=express();
 
 app.use(express.static(publicPath));
 
 
 
-app.listen(3000,()=>
+app.listen(port,()=>
 {
-    console.log('server is running on  port 3000....');
+    console.log('server is running on  port ',port);
 })
 
